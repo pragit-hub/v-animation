bd41766a7eeb9aaa9ec18e8f795f0e8a7ed5c044
pragit-hub <xtremegamer8126@gmail.com>
2025-02-05 15:23:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 window.requestAnimationFrame =
     window.__requestAnimationFrame ||
-        window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        (function () {
-            return function (callback, element) {
-                var lastTime = element.__lastTime;
-                if (lastTime === undefined) {
-                    lastTime = 0;
-                }
-                var currTime = Date.now();
-                var timeToCall = Math.max(1, 33 - (currTime - lastTime));
-                window.setTimeout(callback, timeToCall);
-                element.__lastTime = currTime + timeToCall;
-            };
-        })();
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    (function () {
+        return function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+    })();
 
-window.isDevice = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(((navigator.userAgent || navigator.vendor || window.opera)).toLowerCase()));
+window.isDevice = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+
 var loaded = false;
 var init = function () {
     if (loaded) return;
     loaded = true;
+    
     var mobile = window.isDevice;
-    var koef = mobile ? 1 : 1;
     var canvas = document.getElementById('heart');
     var ctx = canvas.getContext('2d');
-    var width = canvas.width = koef * innerWidth;
-    var height = canvas.height = koef * innerHeight;
+
+    // Scale for high-DPI screens
+    var scale = window.devicePixelRatio || 1;
+    canvas.width = innerWidth * scale;
+    canvas.height = innerHeight * scale;
+    canvas.style.width = innerWidth + "px";
+    canvas.style.height = innerHeight + "px";
+    ctx.scale(scale, scale);
+
+    var width = canvas.width / scale;
+    var height = canvas.height / scale;
     var rand = Math.random;
-    ctx.fillStyle = "rgba(0,0,0,1)";
+
+    ctx.fillStyle = "rgba(0, 0, 0, 1)";
     ctx.fillRect(0, 0, width, height);
 
     var heartPosition = function (rad) {
@@ -42,40 +46,35 @@ var init = function () {
     };
 
     window.addEventListener('resize', function () {
-        width = canvas.width = koef * innerWidth;
-        height = canvas.height = koef * innerHeight;
-        ctx.fillStyle = "rgba(0,0,0,1)";
-        ctx.fillRect(0, 0, width, height);
+        init(); // Reinitialize on resize for sharpness
     });
 
-    var traceCount = mobile ? 20 : 60;
+    var traceCount = mobile ? 30 : 60;
     var size1 = mobile ? 100 : 210;
     var size2 = mobile ? 6 : 13;
-
-    var size11 = mobile ? 80 : 150;
-    var size12 = mobile ? 3 : 9;
+    
     var pointsOrigin = [];
-    var i;
-    var dr = mobile ? 0.3 : 0.1;
-    for (i = 0; i < Math.PI * 2; i += dr) pointsOrigin.push(scaleAndTranslate(heartPosition(i), size1, size2, 0, 0));
-    for (i = 0; i < Math.PI * 2; i += dr) pointsOrigin.push(scaleAndTranslate(heartPosition(i), size11, size12, 0, 0));
-    for (i = 0; i < Math.PI * 2; i += dr) pointsOrigin.push(scaleAndTranslate(heartPosition(i), 90, 5, 0, 0));
+    for (var i = 0; i < Math.PI * 2; i += 0.1) {
+        pointsOrigin.push(scaleAndTranslate(heartPosition(i), size1, size2, 0, 0));
+    }
+    
     var heartPointsCount = pointsOrigin.length;
-
     var targetPoints = [];
+
     var pulse = function (kx, ky) {
-        for (i = 0; i < pointsOrigin.length; i++) {
-            targetPoints[i] = [];
-            targetPoints[i][0] = kx * pointsOrigin[i][0] + width / 2;
-            targetPoints[i][1] = ky * pointsOrigin[i][1] + height / 2;
+        for (var i = 0; i < pointsOrigin.length; i++) {
+            targetPoints[i] = [
+                kx * pointsOrigin[i][0] + width / 2,
+                ky * pointsOrigin[i][1] + height / 2
+            ];
         }
     };
 
-    var e = [];
-    for (i = 0; i < heartPointsCount; i++) {
+    var particles = [];
+    for (var i = 0; i < heartPointsCount; i++) {
         var x = rand() * width;
         var y = rand() * height;
-        e[i] = {
+        particles[i] = {
             vx: 0,
             vy: 0,
             R: 2,
@@ -83,10 +82,9 @@ var init = function () {
             q: ~~(rand() * heartPointsCount),
             D: 2 * (i % 2) - 1,
             force: 0.2 * rand() + 0.7,
-            f: "hsla(0, " + ~~(40 * rand() + 60) + "%, " + ~~(60 * rand() + 20) + "%, .5)", // Valentine color scheme with shades of red
-            trace: []
+            f: "hsl(" + ~~(rand() * 20 + 350) + ", 100%, " + ~~(rand() * 30 + 50) + "%)",
+            trace: new Array(traceCount).fill({x, y})
         };
-        for (var k = 0; k < traceCount; k++) e[i].trace[k] = { x: x, y: y };
     }
 
     var config = {
@@ -99,52 +97,56 @@ var init = function () {
         var n = -Math.cos(time);
         pulse((1 + n) * 0.5, (1 + n) * 0.5);
         time += ((Math.sin(time)) < 0 ? 9 : (n > 0.8) ? 0.2 : 1) * config.timeDelta;
-        ctx.fillStyle = "rgba(0,0,0,.1)";
+
+        ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
         ctx.fillRect(0, 0, width, height);
-        for (i = e.length; i--;) {
-            var u = e[i];
-            var q = targetPoints[u.q];
-            var dx = u.trace[0].x - q[0];
-            var dy = u.trace[0].y - q[1];
+
+        for (var i = particles.length; i--;) {
+            var p = particles[i];
+            var q = targetPoints[p.q];
+            var dx = p.trace[0].x - q[0];
+            var dy = p.trace[0].y - q[1];
             var length = Math.sqrt(dx * dx + dy * dy);
-            if (10 > length) {
+
+            if (length < 10) {
                 if (0.95 < rand()) {
-                    u.q = ~~(rand() * heartPointsCount);
-                }
-                else {
+                    p.q = ~~(rand() * heartPointsCount);
+                } else {
                     if (0.99 < rand()) {
-                        u.D *= -1;
+                        p.D *= -1;
                     }
-                    u.q += u.D;
-                    u.q %= heartPointsCount;
-                    if (0 > u.q) {
-                        u.q += heartPointsCount;
+                    p.q += p.D;
+                    p.q %= heartPointsCount;
+                    if (p.q < 0) {
+                        p.q += heartPointsCount;
                     }
                 }
             }
-            u.vx += -dx / length * u.speed;
-            u.vy += -dy / length * u.speed;
-            u.trace[0].x += u.vx;
-            u.trace[0].y += u.vy;
-            u.vx *= u.force;
-            u.vy *= u.force;
-            for (k = 0; k < u.trace.length - 1;) {
-                var T = u.trace[k];
-                var N = u.trace[++k];
+
+            p.vx += -dx / length * p.speed;
+            p.vy += -dy / length * p.speed;
+            p.trace[0].x += p.vx;
+            p.trace[0].y += p.vy;
+            p.vx *= p.force;
+            p.vy *= p.force;
+
+            for (var k = 0; k < p.trace.length - 1;) {
+                var T = p.trace[k];
+                var N = p.trace[++k];
                 N.x -= config.traceK * (N.x - T.x);
                 N.y -= config.traceK * (N.y - T.y);
             }
-            ctx.fillStyle = u.f;
-            for (k = 0; k < u.trace.length; k++) {
-                ctx.fillRect(u.trace[k].x, u.trace[k].y, 1, 1);
+
+            ctx.fillStyle = p.f;
+            for (k = 0; k < p.trace.length; k++) {
+                ctx.fillRect(p.trace[k].x, p.trace[k].y, 2, 2);
             }
         }
-
-        window.requestAnimationFrame(loop, canvas);
+        window.requestAnimationFrame(loop);
     };
     loop();
 };
 
-var s = document.readyState;
-if (s === 'complete' || s === 'loaded' || s === 'interactive') init();
+if (document.readyState !== 'loading') init();
 else document.addEventListener('DOMContentLoaded', init, false);
+
